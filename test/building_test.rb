f36ebe2ca3600d7b8ed10383963c1a2f61bdb1f4
Phil McCarthy <philmcc2631@gmail.com>
2020-10-20 09:53:01 -0600

    Add building test for average_rent method

--- test/building_test.rb
+++ test/building_test.rb
@@ -61,4 +61,27 @@ class BuildingTest < Minitest::Test
     unit2.add_renter(renter2)
     assert_equal ["Aurora", "Tim"], building.renters
   end
+
+  def test_it_can_report_average_rent
+    building = Building.new
+    unit1 = Apartment.new(
+                            {
+                              number: "A1",
+                              monthly_rent: 1200,
+                              bathrooms: 1,
+                              bedrooms: 1
+                            }
+                         )
+    unit2 = Apartment.new(
+                            {
+                              number: "B2",
+                              monthly_rent: 999,
+                              bathrooms: 2,
+                              bedrooms: 2
+                            }
+                          )
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    assert_equal 1099.5, building.average_rent
+  end
 end
