9705a84733f9ba2f5dca559149735941735ef079
Phil McCarthy <philmcc2631@gmail.com>
2020-10-20 09:26:18 -0600

    Implement add_unit method in building class

--- lib/building.rb
+++ lib/building.rb
@@ -1,6 +1,11 @@
 class Building
-  attr_reader :units
+  attr_reader :units, :renters
   def initialize
     @units = []
+    @renters = []
+  end
+
+  def add_unit(unit)
+    @units << unit
   end
 end
