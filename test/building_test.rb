c769efc09a2976a7f960b1959e67a59181736c05
Phil McCarthy <philmcc2631@gmail.com>
2020-10-20 10:33:53 -0600

    Add test case for annual breakdown

--- test/building_test.rb
+++ test/building_test.rb
@@ -137,4 +137,28 @@ class BuildingTest < Minitest::Test
                 }
     assert_equal expected, building.units_by_number_of_bedrooms
   end
+
+  def test_annual_breakdown
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    renter1 = Renter.new("Spencer")
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+    unit2.add_renter(renter1)
+
+    expected = {"Spencer" => 11988}
+    assert_equal expected, building.annual_breakdown
+
+    renter2 = Renter.new("Jessie")
+    unit1.add_renter(renter2)
+
+    expected = {"Jessie" => 14400, "Spencer" => 11988}
+    assert_equal expected, building.annual_breakdown
+  end
+  # building.rooms_by_renter
+  #=> {<Renter:0x00007fb333af5a80...> => {bathrooms: 1, bedrooms: 1},
+  #    #<Renter:0x00007fb333d0d7f0...> => {bathrooms: 2, bedrooms: 2}}
 end
