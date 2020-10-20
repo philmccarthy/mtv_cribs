998d76cc3e7b7280c6677cefb185ab9ecd9fe5ed
Phil McCarthy <philmcc2631@gmail.com>
2020-10-20 09:52:33 -0600

    Update renters method to report renter names

--- lib/building.rb
+++ lib/building.rb
@@ -1,11 +1,20 @@
 class Building
-  attr_reader :units, :renters
+  attr_reader :units
   def initialize
     @units = []
-    @renters = []
   end
 
   def add_unit(unit)
     @units << unit
+  end
+
+  def renters
+    @renters = []
+      @units.each do |unit|
+        if unit.renter != nil
+          @renters << unit.renter.name
+        end
+      end
+    @renters
   end
 end
